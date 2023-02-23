1e460edd2d865bb2d6375e1236dd8916390f70d0
thanhhv91 <vanthanh721991@gmail.com>
2023-02-23 23:15:49 +0700

    the second commit

---
+++ build/CMakeFiles/CMakeStaterCpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeStaterCpp.dir/main.cpp.obj"
+  "CMakeFiles/CMakeStaterCpp.dir/main.cpp.obj.d"
+  "CMakeStaterCpp.exe"
+  "CMakeStaterCpp.exe.manifest"
+  "CMakeStaterCpp.pdb"
+  "libCMakeStaterCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeStaterCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
