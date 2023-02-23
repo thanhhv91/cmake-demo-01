8c91dc8b93e5cb8f0835fdb82b426686dfe31f7e
thanhhv91 <vanthanh721991@gmail.com>
2023-02-24 05:15:58 +0700

    success fully

---
+++ build/CMakeFiles/cpplib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Study/source/repos/cmake-demo-01/main.cpp" "CMakeFiles/cpplib.dir/main.obj" "gcc" "CMakeFiles/cpplib.dir/main.obj.d"
+  "D:/Study/source/repos/cmake-demo-01/src/cpplib.cpp" "CMakeFiles/cpplib.dir/src/cpplib.obj" "gcc" "CMakeFiles/cpplib.dir/src/cpplib.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cpplib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplib.dir/main.obj"
+  "CMakeFiles/cpplib.dir/main.obj.d"
+  "CMakeFiles/cpplib.dir/src/cpplib.obj"
+  "CMakeFiles/cpplib.dir/src/cpplib.obj.d"
+  "cpplib.exe"
+  "cpplib.exe.manifest"
+  "cpplib.pdb"
+  "libcpplib.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
